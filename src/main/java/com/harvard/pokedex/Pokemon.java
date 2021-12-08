0255b310c839052313f53e32c99f2c5ffac51271
Rafael Santos <64181619+Rafaelse6@users.noreply.github.com>
2021-12-08 00:42:27 -0300

    Add files via upload
    Pokedex

---
+++ src/main/java/com/harvard/pokedex/Pokemon.java
@@ -0,0 +1,18 @@
+package com.harvard.pokedex;
+
+public class Pokemon {
+    private String name;
+    private int number;
+
+    Pokemon(String name, int number){
+        this.name = name;
+        this.number = number;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+    public int getNumber(){
+        return this.number;
+    }
+}
